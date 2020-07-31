1223a9fa3a8611b56c06d8ebcd5421bebd36c891
zhangxu668 <1505046353@qq.com>
2020-07-31 20:14:32 +0800

    1

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,6 @@
 import user from './user'
 import home from './home'
-<<<<<<< HEAD
 import discuss from './discuss'
-export default {
-  user,
-  home,
-  discuss,
-=======
 import hospital from './hospital'
 import patients from './patient'
 export default {
@@ -14,5 +8,5 @@ export default {
   home,
   patients,
   hospital,
->>>>>>> 2c2e1e9636a7fb0654cc9733d21f6288f885878a
+  discuss,
 }
