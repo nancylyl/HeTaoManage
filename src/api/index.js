1cf582687b833f81e2f22c967bca6cd77c38551d
e <yalan.liu@macrowing.com>
2020-08-03 21:57:08 +0800

    新增基本病历，就诊记录，发作次数纪录，症状纪录，症状日志，查看病历详情

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import home from './home'
 import discuss from './discuss'
 import hospital from './hospital'
 import patients from './patient'
-import addCase from './addCase'
+import addCase from './case'
 import map from './map'
 export default {
   user,
